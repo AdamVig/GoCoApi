a8629b945e3a3acebc49eb34635c1b9fc374a2bc
Adam Vigneaux <adamvigneaux@gmail.com>
2016-01-20 19:50:02 -0500

    Add environment variables file

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const config = require('./config');
 const utils = require('./utils');
 const responseFormatter = require('./response-formatter');
+const fs = require('fs');
 const restify = require('restify');
+
+// Check for existence of environment variables file
+try {
+    fs.accessSync("./vars.js", fs.F_OK);
+} catch (e) {
+    throw new Error("Could not start server: No vars file found. " +
+        "Check README for instructions.");
+}
 
 const app = restify.createServer({
     name: config.APP_NAME,
---
+++ vars-template.js
@@ -0,0 +1,10 @@
+module.exports = vars = {};
+vars.db = {
+    name: "",    // Name of database
+    url: "",     // URL to access database at (ex: accountname.cloudant.com)
+    user: "",    // API key username
+    password: "" // API key password
+};
+vars.forecastio = {
+    key: "" // Register for an API key at https://developer.forecast.io/
+};
