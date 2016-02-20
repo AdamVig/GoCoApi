abd2e7cc4d9b100928c7ff01952d486f5a194eca
Adam Vigneaux <adamvigneaux@gmail.com>
2016-02-19 23:39:09 -0500

    Add documentation to getGoGordonSecure function

--- helpers/getters.js
+++ helpers/getters.js
@@ -15,6 +15,14 @@ getters.getGoGordon = function (url, auth) {
     return request({url: url, auth: auth, jar: true, transform: cheerio.load});
 };
 
+/**
+ * Get a page from Go Gordon that requires reauthentication
+ * Sets reauthentication cookie, then lets getGoGordon handle getting
+ * the actual page.
+ * @param  {string}  url   Partial URL, ex: general/whoami.cfm
+ * @param  {object}  auth  Contains username and password in plaintext
+ * @return {promise}       Resolved by result of getGoGordon() for same URL
+ */
 getters.getGoGordonSecure = function (url, auth) {
     const reauthURL = "https://go.gordon.edu/lib/auth/level3logon.cfm";
     const form = {
