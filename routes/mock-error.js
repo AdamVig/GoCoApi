7dc752aded4e5cc038c888b9e4f0b7bc77187f82
Adam Vigneaux <adamvigneaux@gmail.com>
2016-06-19 21:38:53 -0400

    Refactor to support new error format
    - Use the standard endpoint creation function instead of custom one
    - Add getter and processor functions for compatibility with the endpoint
      creation function

--- routes/mock-error.js
+++ routes/mock-error.js
@@ -1,3 +1,4 @@
+const restify = require('restify');
 const config = require('../config.js');
 
 module.exports = routeMockError = {};
@@ -8,20 +9,40 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
+/**
+ * Get error codes from configuration
+ * Note: this method is not async, but returns a Promise because
+ *       endpoint.make() expects it to
+ * @return {promise} Resolves to array containing error codes as numbers
+ */
+routeMockError.getErrorCodes = function () {
+    return Promise.resolve(Object.keys(config.ERROR)
+                               .map((code) => parseInt(code, 10)));
+};
+
+/**
+ * Pick a random error
+ * Note: this method is not async, but returns a Promise because
+ *       endpoint.make() expects it to
+ * @param {array} errorCodes Contains error codes as numbers
+ * @return {promise}         Rejects with random error
+ */
+routeMockError.pickRandomError = function (errorCodes) {
+    const randomIndex = getRandomInt(0, errorCodes.length - 1);
+    const errorCode = errorCodes[randomIndex];
+    const error = restify.errors.codeToHttpError(errorCode);
+    error.message = error.body.message = "This is a random error message.";
+
+    return Promise.reject(error);
+};
+
 routeMockError.ENDPOINT = {
-  name: "mockerror"
+    getter: routeMockError.getErrorCodes,
+    method: "get",
+    name: "mockerror",
+    processor: routeMockError.pickRandomError
 };
 
 routeMockError.endpoint = (app) => {
-    app.get(config.PREFIX + routeMockError.ENDPOINT.name,
-            (req, res, next) => {
-
-        const errorCodes = Object.keys(config.ERROR);
-        const randomIndex = getRandomInt(0, errorCodes.length - 1);
-        const errorCode = errorCodes[randomIndex];
-
-        // Send response
-        res.status(parseInt(errorCode, 10));
-        res.send(config.ERROR[errorCode]);
-    });
+    endpoint.make(app, routeMockError.ENDPOINT);
 };
