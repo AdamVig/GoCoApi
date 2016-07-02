f240a4d668d962de76ed2472851418f88906ee23
Adam Vigneaux <adamvigneaux@gmail.com>
2016-07-01 22:10:41 -0400

    Refactor meal points per day to use destructuring
    Instead of using positional values in the array passed to the function,
    make use of destructuring to assign names to the data.

--- routes/meal-points-per-day.js
+++ routes/meal-points-per-day.js
@@ -1,6 +1,15 @@
 const routeDaysLeft = require("./days-left-in-semester");
 const routeMealPoints = require("./meal-points");
 
+/**
+ * Get meal points per day
+ * Uses the getter and processor functions from both meal points and days left
+ *     in semester routes to prevent unncessary code duplication.
+ * @param {string} location Unused
+ * @param {object} auth Contains username and password in plaintext
+ * @return {Promise}    Fulfilled by array containing meal points and
+ *                      days left in semester
+ */
 function getMealPointsPerDay(location, auth) {
     const dataRequests = [
         routeMealPoints.getter("", auth)
@@ -19,7 +28,8 @@ function getMealPointsPerDay(location, auth) {
  * @return {number}      Meal points, rounded to the nearest dollar
  */
 function calculateMealPointsPerDay(data) {
-    return Math.round(data[0] / data[1]);
+    const [mealPoints, daysLeftInSemester] = data;
+    return Math.round(mealPoints / daysLeftInSemester);
 }
 
 module.exports = {
