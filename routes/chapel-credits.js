1bc4ae9f497d5e4a7b19ca0f20d2ba3e5c38c047
Adam Vigneaux <adamvigneaux@gmail.com>
2016-07-19 21:20:24 -0400

    Add error handling for no chapel credits found
    Handles the edge case in which a message is displayed on the chapel
    credits page.

--- routes/chapel-credits.js
+++ routes/chapel-credits.js
@@ -2,17 +2,25 @@ const restify = require("restify");
 
 const getters = require("../helpers/getters");
 
+const noChapelCreditMessage = "No Christian Life and Worship Credit Found";
+
 /**
  * Get chapel credits from page
  * @param  {cheerio} $ Cheerio page object
  * @return {number}    Chapel credits
  */
 function getChapelCredits($) {
+
+    // Return zero if message is displayed
+    if ($.html().includes(noChapelCreditMessage)) {
+        return 0;
+    }
+
     const dataString = $("body").find("table")
-        .last()
-        .children().first()
-        .children().last()
-        .text();
+              .last()
+              .children().first()
+              .children().last()
+              .text();
 
     if (dataString.length === 0 || !dataString) {
         throw new restify.BadGatewayError(
